fdee2621eb4082a60179fb4fd01c05c72273ec08
gopi-ceq <121539215+gopi-ceq@users.noreply.github.com>
2023-01-09 13:19:53 +0530

    Update JP.java

--- JP.java
+++ JP.java
@@ -4,8 +4,15 @@ public class JP {
     public static void main(String[] args) {
         System.out.println(
             String.format("The current shell is: %s.", System.getenv("SHELL"))
+           
+        );
+        System.out.println(
             String.format("The current shell is: %s.", System.getenv("INPUT_NAME"))
+           
+        );
+        System.out.println(
             String.format("The current shell is: %s.", System.getenv("INPUT_MESSAGE"))
+           
         );
     }
 }
