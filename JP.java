27e7647e08a682bb49ae46d844f4a74d9e8a3267
gopi-ceq <121539215+gopi-ceq@users.noreply.github.com>
2023-01-09 13:42:30 +0530

    Update JP.java

--- JP.java
+++ JP.java
@@ -3,15 +3,15 @@ package com.settermjd.twilio.envvars;
 public class JP {
     public static void main(String[] args) {
         System.out.println(
-            String.format("The current shell is: %s.", System.getenv("$SHELL"))
+            String.format("The current shell is: %s.", System.getenv("SHELL"))
            
         );
         System.out.println(
-            String.format("The current shell is: %s.", System.getenv("$INPUT_NAME"))
+            String.format("The current shell is: %s.", System.getenv("INPUT_NAME"))
            
         );
         System.out.println(
-            String.format("The current shell is: %s.", System.getenv("$INPUT_MESSAGE"))
+            String.format("The current shell is: %s.", System.getenv("INPUT_MESSAGE"))
            
         );
     }
